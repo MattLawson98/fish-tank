607ed75fe69ad2186fafa5b39a531f5b9e67ae85
MattLawson98 <matthewlawson50@gmail.com>
2022-10-14 14:04:52 -0700

    Started fishtank web browser project

---
+++ public/scripts/entities/bite_fish.js
@@ -0,0 +1,6 @@
+class BiteFish extends Fish {
+  constructor(options) {
+    super(options); // Call super to run the code inside `Fish`'s constructor
+    this.imageUri = '/images/BiteFish.gif'; // Set the image
+  }
+}
